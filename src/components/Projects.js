2f6e39aee1b8729bbb8e7d6bcf2dc2823b3a2e44
eylulkadioglu <ekadiog1@binghamton.edu>
2025-05-17 15:07:30 -0400

    WIP: local customization before rebase

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/heartofny.png";
-import projImg2 from "../assets/img/project-img2.png";
+import projImg2 from "../assets/img/rxrefund.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
